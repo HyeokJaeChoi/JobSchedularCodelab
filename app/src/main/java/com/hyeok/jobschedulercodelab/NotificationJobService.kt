4635f9f87da67de77b0ba25cce6d4cefdf9e37fc
최혁재 <gurwo1007@gmail.com>
2020-01-03 18:23:22 +0900

    Job을 실행할 JobService 구현체 추가

---
+++ app/src/main/java/com/hyeok/jobschedulercodelab/NotificationJobService.kt
@@ -0,0 +1,14 @@
+package com.hyeok.jobschedulercodelab
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+
+class NotificationJobService: JobService() {
+    override fun onStartJob(p0: JobParameters?): Boolean {
+
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+
+    }
+}
