af0a4464249926889d681a8ddea7bbba5443c840
최혁재 <gurwo1007@gmail.com>
2020-01-08 11:13:41 +0900

    네트워크 제약 사항이 있는 JobService 추가

--- app/src/main/java/com/hyeok/jobschedulercodelab/MainActivity.kt
+++ app/src/main/java/com/hyeok/jobschedulercodelab/MainActivity.kt
@@ -1,12 +1,74 @@
 package com.hyeok.jobschedulercodelab
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.getSystemService
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val jobScheduler by lazy { getSystemService<JobScheduler>() }
+    private val JOB_ID = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scheduleJobBtn.setOnClickListener(this)
+        cancelJobBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        p0?.let {
+            when(it.id) {
+                R.id.scheduleJobBtn -> {
+                    scheduleJob()
+                }
+                R.id.cancelJobBtn -> {
+                    cancelJob()
+                }
+            }
+        }
+    }
+
+    private fun scheduleJob() {
+        val selectedNetworkId = networkOptions.checkedRadioButtonId
+        val selectedNetworkOption = when(selectedNetworkId) {
+            R.id.noNetwork -> {
+                JobInfo.NETWORK_TYPE_NONE
+            }
+            R.id.anyNetwork -> {
+                JobInfo.NETWORK_TYPE_ANY
+            }
+            R.id.wifiNetwork -> {
+                JobInfo.NETWORK_TYPE_UNMETERED
+            }
+            else -> {
+                JobInfo.NETWORK_TYPE_NONE
+            }
+        }
+        val componentName = ComponentName(packageName, NotificationJobService::class.java.name)
+        val jobInfoBuilder = JobInfo.Builder(JOB_ID, componentName)
+            .setRequiredNetworkType(selectedNetworkOption)
+        val constraintSet = selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE
+
+        if(constraintSet) {
+            val jobInfo = jobInfoBuilder.build()
+            jobScheduler?.schedule(jobInfo)
+            Toast.makeText(this, "Job Scheduled, job will run when the constraints are met.", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(this, "Please set at least one constraint", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun cancelJob() {
+        jobScheduler?.cancelAll()
+        Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/hyeok/jobschedulercodelab/NotificationJobService.kt
+++ app/src/main/java/com/hyeok/jobschedulercodelab/NotificationJobService.kt
@@ -1,14 +1,62 @@
 package com.hyeok.jobschedulercodelab
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.getSystemService
 
 class NotificationJobService: JobService() {
-    override fun onStartJob(p0: JobParameters?): Boolean {
 
+    private val notificationManager by lazy { getSystemService<NotificationManager>() }
+    private val PRIMARY_CHANNEL_ID by lazy { "primary_notification_channel" }
+
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        createNotificationChannel()
+
+        val contentPendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            Intent(this, MainActivity::class.java),
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val notificationBuilder = NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+            .setContentTitle("Job Service")
+            .setContentText("Your Job ran to completion!")
+            .setContentIntent(contentPendingIntent)
+            .setSmallIcon(R.drawable.ic_job_running)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
+
+        notificationManager?.notify(0, notificationBuilder.build())
+
+        return false
     }
 
     override fun onStopJob(p0: JobParameters?): Boolean {
+        return true
+    }
 
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                "Job Service Notification",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                description = "Notifications from Job Service"
+            }
+
+            notificationManager?.createNotificationChannel(notificationChannel)
+        }
     }
 }
